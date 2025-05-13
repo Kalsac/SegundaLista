b7128f5b33a38c7c47cc371564aa82c95d60309c
Isaac/ Ketellin <oliveira.ketellin@escola.pr.gov.br>
2025-05-13 11:30:34 +0000

    finalizado

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ let nomes = ['Isaac', 'Luca', 'Vitoria'];
 console.log(nomes);
 console.log("O tamanho do Array nomes é: " + nomes.length);
 
-
 let nomes1 = ["Isaac", "Luca"];
 nomes1.push("Vippy");
 nomes1.push("Pedro");
 console.log(nomes1);
-
 
 let nomes2 = ['Isaac', 'Luca', 'Vippy', 'Pedro'];
 nomes2.unshift("Mahara");
@@ -19,27 +17,39 @@ let ultimoNomeRemovido = nomes3.pop();
 console.log(`Nome removido: ${ultimoNomeRemovido}`);
 console.log(`Array restante: ${nomes3}`);
 
+let nome4 = [`Isaac`, `Luca`, `Vippy`, `Mahara`, `Pedro`]
+let primeiroNomeRemovido = nome4.shift();
+console.log(`Nome removido: ${primeiroNomeRemovido}`);
+console.log(`Array restante: ${nome4}`);
 
-// EXERCÍCIO 05 - Removendo o primeiro
-// Remova o primeiro nome do array e exiba o nome removido e o array restante.
-// Método sugerido: shift
+let frutas = [`uva`, `banana`, `maça`, `manga`, `mamão`]
+let removerDuasFrutas = frutas.splice();
+console.log(`frutas removidas: ${removerDuasFrutas}`)
+console.log(`Array restante: ${frutas}`);
 
-// EXERCÍCIO 06 - Removendo e adicionando elementos
-// Crie um array com cinco frutas. Remova duas frutas a partir da segunda posição e adicione duas novas frutas no lugar.
-// Método sugerido: splice
 
-// EXERCÍCIO 07 - Exibindo o tamanho do array
-// Exiba o tamanho do array de frutas após a alteração.
-// Método sugerido: length
+console.log(frutas);
+console.log("O tamanho do Array frutas é: " + frutas.length);
 
-// EXERCÍCIO 08 - Criando um histórico de páginas
-// Crie um array chamado historico e simule acessos às páginas "home", "sobre" e "contato".
-// Método sugerido: push
 
-// EXERCÍCIO 09 - Removendo o último acesso do histórico
-// Remova o último acesso do histórico e exiba "Saída da página: contato".
-// Método sugerido: pop
+let historico = [];
+historico.push("home");
+console.log("Acessou: home");
+console.log("Histórico:", historico);
+historico.push("sobre");
+console.log("Acessou: sobre");
+console.log("Histórico:", historico);
+historico.push("contato");
+console.log("Acessou: contato");
+console.log("Histórico:", historico);
 
-// EXERCÍCIO 10 - Alterando elementos
-// Crie um array de quatro cores e troque a terceira cor por "azul-marinho".
-// Método sugerido: splice
+
+let historico1 = ["home", "sobre", "contato"];
+let ultimoAcesso = historico1.pop();
+console.log("Saída da página:", ultimoAcesso);
+console.log("Histórico:", historico1);
+
+
+let cores = ["vermelho", "verde", "amarelo", "roxo"];
+cores.splice(2, 1, "azul-marinho");
+console.log("Novo array de cores:", cores);
